947434260b29f3f82e464485ac5917cddd880686
Tim-Huo <huowencheng@yeah.net>
2020-10-27 10:53:33 +0800

    将ArticleController类添加注释

--- service-topic/src/main/java/com/dataint/topic/controller/ArticleController.java
+++ service-topic/src/main/java/com/dataint/topic/controller/ArticleController.java
@@ -9,14 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 专题文章
+ *
+ * @auther: Tim_Huo
+ * @date: 2020/10/26 5:51 下午
+ */
 @RestController
 @RequestMapping(value = "/article")
 public class ArticleController {
     @Autowired
     private IArticleService articleService;
 
+    /**
+     * 查询所有专题文章
+     *
+     * @auther: Tim_Huo
+     * @param: ArticleConditionForm
+     * @return: ResultVO
+     * @date: 2020/10/27 10:52 上午
+     */
     @GetMapping("/getArticleList")
-    @ResponseBody
     public ResultVO getArticleList(ArticleConditionForm acReq) {
 
         return ResultVO.success(articleService.queryArticlesByCondition(acReq));
