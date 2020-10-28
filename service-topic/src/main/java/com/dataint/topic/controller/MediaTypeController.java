2f08b2b43f4f7d80d4158c9fe7ab2b3defb2f44d
Tim-Huo <huowencheng@yeah.net>
2020-10-28 11:22:01 +0800

    将MediaTypeController中方法添加注释

--- service-topic/src/main/java/com/dataint/topic/controller/MediaTypeController.java
+++ service-topic/src/main/java/com/dataint/topic/controller/MediaTypeController.java
@@ -2,6 +2,7 @@ package com.dataint.topic.controller;
 
 import com.dataint.cloud.common.model.ResultVO;
 import com.dataint.topic.service.IMediaTypeService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,14 @@ public class MediaTypeController {
     @Autowired
     private IMediaTypeService mediaTypeService;
 
+    /**
+     * 查询媒体类型
+     *
+     * @auther: Tim_Huo
+     * @return: ResultVO
+     * @date: 2020/10/28 11:00 上午
+     */
+    @ApiOperation(value = "查询媒体类型", notes = "查询媒体类型")
     @GetMapping("/getMediaTypeList")
     @ResponseBody
     public ResultVO getMediaTypeList() {
